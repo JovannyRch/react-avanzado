b68c4ec8c75d2782f590657a8598e1d2e9b9f1ff
jovannyrch <jovannyrch@gmail.com>
2020-08-12 10:03:04 -0500

    custom hooks

--- src/components/Category/ListOfCategories/index.js
+++ src/components/Category/ListOfCategories/index.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Category from "..";
 import { List, Item } from "./styles";
-import { categories } from "../../../../api/db.json";
+
+const useSetCategories = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    window
+      .fetch("https://petgram-server-24iykciv5.now.sh/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setCategories(data);
+      });
+  }, []);
+  return { categories, loading };
+};
 
 const ListOfCategories = () => {
-  return (
-    <List>
+  const [showFixed, setShowFixed] = useState(false);
+  const { categories, loading } = useSetCategories();
+
+  useEffect(
+    function () {
+      const onScroll = (e) => {
+        const newShowFixed = window.scrollY > 200;
+        showFixed !== newShowFixed && setShowFixed(newShowFixed);
+      };
+
+      document.addEventListener("scroll", onScroll);
+
+      return () => document.removeEventListener("scroll", onScroll);
+    },
+    [showFixed]
+  );
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {categories.map((category) => (
-        <Item key={category}>
+        <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  );
+
+  if (loading) return <h2>Cargando</h2>;
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
 
