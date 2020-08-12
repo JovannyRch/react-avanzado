f44a0c2d0314bc1d8ad2fcfbbc6d2497e2edf0e8
jovannyrch <jovannyrch@gmail.com>
2020-08-11 23:06:14 -0500

    PhotoCard Component

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Wrapper, Image, Button } from "./styles";
+import { MdFavoriteBorder } from "react-icons/md";
 
-const PhotoCard = ({ likes = 0, id, src }) => {
+const DEFAULT_IMG =
+  "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg";
+
+const PhotoCard = ({ likes = 0, id, src = DEFAULT_IMG }) => {
   return (
     <article>
       <a href={"/details/" + id}>
-        <div>
-          <img src={src} />
-        </div>
+        <Wrapper>
+          <Image src={src} />
+        </Wrapper>
       </a>
-      <button>{likes} likes</button>
+      <Button>
+        <MdFavoriteBorder size="32px" />
+        {likes} likes
+      </Button>
     </article>
   );
 };
